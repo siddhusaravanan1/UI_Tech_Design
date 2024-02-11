d9aed9219cdf37f2de898cca6b18d81c36ef73f6
SushiHunter <58287015+siddhusaravanan1@users.noreply.github.com>
2024-02-11 17:47:14 +0530

    Completed First mission scenario without position and added scene for skill tree

--- Assets/MissionReportBehaviour.cs
+++ Assets/MissionReportBehaviour.cs
@@ -16,6 +16,40 @@ public class MissionReportBehaviour : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI subText;
 
+    int subTextIndex = 0;
+    int mainTextIndex = 0;
     //Method to subcribe to the current mission
     //Method to handle the animations
+    private void Start()
+    {
+        mainText.text = missionSystem[mainTextIndex].missionName;
+        subText.text = missionSystem[mainTextIndex].subMissions[subTextIndex];
+
+        Debug.Log(missionSystem[0].subMissions[0]);
+    }
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            NextKey();
+        }
+    }
+    void NextKey()
+    {
+        DisplayNextKey();
+    }
+    void DisplayNextKey()
+    {
+        if (subTextIndex <= missionSystem[mainTextIndex].subMissions.Count)
+        {
+            Debug.Log(missionSystem[mainTextIndex].subMissions[subTextIndex]);
+            subText.text = missionSystem[mainTextIndex].subMissions[subTextIndex];
+            subTextIndex++;
+        }
+        else
+        {
+            Debug.Log("End of List");
+        }
+    }
 }
